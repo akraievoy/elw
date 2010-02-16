4f7c379e54ea67a3480814bd430b585923e21b76
akraievoy <akraievoy@1335212e-3b97-4035-a47e-bf61190ede09>
2010-02-16 20:12:06 +0000

    re #172: tasks now automatically update
    git-svn-id: http://akraievoy.unfuddle.com/svn/akraievoy_me/trunk@56 1335212e-3b97-4035-a47e-bf61190ede09

--- iterate.sh
+++ iterate.sh
@@ -2,7 +2,8 @@ while echo '------------->' && sleep 5; do {
   cd /home/anton/Documents/projects/mipsEmu;
   if sudo -u anton mvn clean package; then
     rm /var/lib/jetty/webapps/elw.war;
-    cp web/target/web-1.3.war /var/lib/jetty/webapps/elw.war;
+    cp -v web/target/web-1.3.war /var/lib/jetty/webapps/elw.war;
+    cp -v datapath/src/main/resources/aos-s10.json /usr/share/jetty/elw-data/courses;
     service jetty stop
     service jetty start
     read;
