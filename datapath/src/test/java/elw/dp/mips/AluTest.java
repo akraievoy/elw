81f7618575a7321648bb73372d89380ebf8ac8bb
akraievoy <akraievoy@1335212e-3b97-4035-a47e-bf61190ede09>
2011-03-17 07:22:54 +0000

    a simple sample test for ALU
    git-svn-id: http://akraievoy.unfuddle.com/svn/akraievoy_me/trunk@292 1335212e-3b97-4035-a47e-bf61190ede09

--- datapath/src/test/java/elw/dp/mips/AluTest.java
+++ datapath/src/test/java/elw/dp/mips/AluTest.java
@@ -1,10 +1,14 @@
 package elw.dp.mips;
 
+import base.pattern.Result;
 import junit.framework.TestCase;
 
 import java.lang.reflect.Method;
 
 public class AluTest extends TestCase {
+	protected InstructionContext iCtx;
+	protected Memory memory;
+
 	public void testAnnotations() {
 		final Method[] aluMethods = Alu.class.getDeclaredMethods();
 
@@ -16,4 +20,40 @@ public class AluTest extends TestCase {
 			assertTrue(m.getName(), descAnnot.syntax().startsWith(m.getName()));
 		}
 	}
+
+	private MipsValidator validator;
+	protected Registers registers;
+	private Alu alu;
+
+	public void setUp() {
+		validator = new MipsValidator();
+		alu = validator.getDataPath().getAlu();
+		registers = validator.getDataPath().getRegisters();
+		memory = validator.getDataPath().getMemory();
+		iCtx = new InstructionContext(
+				validator.getDataPath().getInstructions(),
+				memory,
+				registers
+		);
+	}
+
+	protected void assembleInstruction(final String instructionCode) {
+		final Instruction instruction = validator.assemble(new Result[1], new String[]{instructionCode})[0];
+		iCtx.setInstruction(instruction);
+	}
+
+	public void testAdd() {
+		registers.setReg(Reg.t2, -1);
+		registers.setReg(Reg.t3, 2);
+		registers.setReg(Reg.t1, 18);
+
+		assembleInstruction("add $t1, $t2, $t3");
+
+		alu.add(iCtx);
+
+		assertEquals(-1, registers.getReg(Reg.t2));
+		assertEquals(2, registers.getReg(Reg.t3));
+		assertEquals(1, registers.getReg(Reg.t1));
+	}
+
 }
