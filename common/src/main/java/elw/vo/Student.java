82e6bb3f61456d807057becdfcb0bff4f7b67adc
akraievoy <akraievoy@gmail.com>
2011-10-25 10:32:14 +0300

    re #6 : found an error on student course index page, we need equals for Student

--- common/src/main/java/elw/vo/Student.java
+++ common/src/main/java/elw/vo/Student.java
@@ -16,4 +16,22 @@ public class Student implements IdNamed {
     private boolean lead;
     public boolean isLead() { return lead; }
     public void setLead(boolean lead) { this.lead = lead; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Student student = (Student) o;
+        return id.equals(student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
