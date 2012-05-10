f13ea406bbe747bbd87631b39b044b282406b7e0
akraievoy <akraievoy@gmail.com>
2012-05-10 07:14:16 +0300

    re #1 : converted js indents to 2 spaces instead of tabs

--- web/src/main/webapp/js/elw.js
+++ web/src/main/webapp/js/elw.js
@@ -22,84 +22,84 @@ var elw_ajaxStamp = 0;
 var elw_updateExpandTriggersUri = null; 
 
 function elw_expand(elemId) {
-	var trigger = jQuery("#" + elemId);
+  var trigger = jQuery("#" + elemId);
 
-	trigger.addClass("expanded");
-	if (trigger.hasClass("ui-icon-triangle-1-e")) {
-		trigger.removeClass("ui-icon-triangle-1-e");
-		trigger.addClass("ui-icon-triangle-1-s");
-	}
-	jQuery("." + elemId).slideDown(0);
-	
+  trigger.addClass("expanded");
+  if (trigger.hasClass("ui-icon-triangle-1-e")) {
+    trigger.removeClass("ui-icon-triangle-1-e");
+    trigger.addClass("ui-icon-triangle-1-s");
+  }
+  jQuery("." + elemId).slideDown(0);
 
-	elw_visible[elemId] = true;
+
+  elw_visible[elemId] = true;
 }
 
 function elw_collapse(elemId) {
-	jQuery("." + elemId).slideUp(0);
-	var trigger = jQuery("#" + elemId);
-	trigger.removeClass("expanded");
-	if (trigger.hasClass("ui-icon-triangle-1-s")) {
-		trigger.removeClass("ui-icon-triangle-1-s");
-		trigger.addClass("ui-icon-triangle-1-e");
-	}
+  jQuery("." + elemId).slideUp(0);
+  var trigger = jQuery("#" + elemId);
+  trigger.removeClass("expanded");
+  if (trigger.hasClass("ui-icon-triangle-1-s")) {
+    trigger.removeClass("ui-icon-triangle-1-s");
+    trigger.addClass("ui-icon-triangle-1-e");
+  }
 
-	elw_visible[elemId] = false;
+  elw_visible[elemId] = false;
 }
 
 function elw_mouseHovered(elemId) {
-	if (elw_enterTimes[elemId] === undefined || elw_enterTimes[elemId] == null) {
-		return;
-	}
-	elw_enterTimes[elemId] = null;
+  if (elw_enterTimes[elemId] === undefined || elw_enterTimes[elemId] == null) {
+    return;
+  }
+  elw_enterTimes[elemId] = null;
 
-	if (elw_visible[elemId] === undefined || !elw_visible[elemId]) {
-		elw_expand(elemId);
-	} else {
-		elw_collapse(elemId);
-		//	process dependent triggers, if any
-		for (var visElemId in elw_visible) {
-			if (visElemId.indexOf(elemId) == 0 && elw_visible[visElemId]) {
-				elw_collapse(visElemId);
-			}
-		}
-	}
+  if (elw_visible[elemId] === undefined || !elw_visible[elemId]) {
+    elw_expand(elemId);
+  } else {
+    elw_collapse(elemId);
+    //	process dependent triggers, if any
+    for (var visElemId in elw_visible) {
+      if (visElemId.indexOf(elemId) == 0 && elw_visible[visElemId]) {
+        elw_collapse(visElemId);
+      }
+    }
+  }
 
-	elw_ajaxStamp++;
-	setTimeout("elw_ajaxStart(" + elw_ajaxStamp + ")", 10000);
+  elw_ajaxStamp++;
+  setTimeout("elw_ajaxStart(" + elw_ajaxStamp + ")", 10000);
 }
 
 function elw_expandedTriggers() {
-	var visElemIds = [];
-	for (var visElemId in elw_visible) {
-		if (elw_visible[visElemId]) {
-			visElemIds.push(visElemId);
-		}
-	}
-	return visElemIds;
+  var visElemIds = [];
+  for (var visElemId in elw_visible) {
+    if (elw_visible[visElemId]) {
+      visElemIds.push(visElemId);
+    }
+  }
+  return visElemIds;
 }
 
 function elw_ajaxStart(myAjaxStamp) {
-	if (elw_ajaxStamp > myAjaxStamp || elw_updateExpandTriggersUri == null) {
-		return;
-	}
+  if (elw_ajaxStamp > myAjaxStamp || elw_updateExpandTriggersUri == null) {
+    return;
+  }
 
-	jQuery.ajax({
-		"type": "POST",
-		"url": elw_updateExpandTriggersUri,
-		"data": jQuery.toJSON(elw_expandedTriggers()),
-		"success": function(resp) {
-			if (resp == null || !resp.success) {
-				jQuery.jGrowl("Failed to save view state", {"header":"Warning", "life": 10000});
-			} else {
-				jQuery.jGrowl("View state saved", {"header":"Info", "life": 3000});
-			}
-		},
-		"error": function() {
-			jQuery.jGrowl("Failed to save view state", {"header":"Warning", "life": 10000});
-		},
-		"dataType": 'json'
-	});
+  jQuery.ajax({
+    "type": "POST",
+    "url": elw_updateExpandTriggersUri,
+    "data": jQuery.toJSON(elw_expandedTriggers()),
+    "success": function(resp) {
+      if (resp == null || !resp.success) {
+        jQuery.jGrowl("Failed to save view state", {"header":"Warning", "life": 10000});
+      } else {
+        jQuery.jGrowl("View state saved", {"header":"Info", "life": 3000});
+      }
+    },
+    "error": function() {
+      jQuery.jGrowl("Failed to save view state", {"header":"Warning", "life": 10000});
+    },
+    "dataType": 'json'
+  });
 }
 
 function elw_dt_url(idx, inner) {
@@ -117,63 +117,63 @@ function elw_dt_urlb(idx, inner) {
 }
 
 jQuery(document).ready(function() {
-	jQuery(".elw_button").button();
-	jQuery(".elw_button_info").button({
-		icons: {
-			primary: "ui-icon-info"
-		}
-	});
-	jQuery(".elw_button_alert").button({
-		icons: {
-			primary: "ui-icon-alert"
-		}
-	});
-	jQuery(".elw_button_logout").button({
-		icons: {
-			primary: "ui-icon-power"
-		}
-	});
-	jQuery(".elw_button_login").button({
-		icons: {
-			primary: "ui-icon-unlocked"
-		}
-	});
-	jQuery(".elw_button_edit").button({
-		icons: {
-			primary: "ui-icon-pencil"
-		}
-	});
-	jQuery(".elw_button_upload").button({
-		icons: {
-			primary: "ui-icon-arrowstop-1-n"
-		}
-	});
+  jQuery(".elw_button").button();
+  jQuery(".elw_button_info").button({
+    icons: {
+      primary: "ui-icon-info"
+    }
+  });
+  jQuery(".elw_button_alert").button({
+    icons: {
+      primary: "ui-icon-alert"
+    }
+  });
+  jQuery(".elw_button_logout").button({
+    icons: {
+      primary: "ui-icon-power"
+    }
+  });
+  jQuery(".elw_button_login").button({
+    icons: {
+      primary: "ui-icon-unlocked"
+    }
+  });
+  jQuery(".elw_button_edit").button({
+    icons: {
+      primary: "ui-icon-pencil"
+    }
+  });
+  jQuery(".elw_button_upload").button({
+    icons: {
+      primary: "ui-icon-arrowstop-1-n"
+    }
+  });
 
  var jQexpand = jQuery(".expandTrigger");
  jQexpand.mouseenter(function() {
-		elw_enterTimes[this.id] = new Date().getTime();
-		setTimeout("elw_mouseHovered('"+this.id+"')", 600);
-	});
-	jQexpand.mouseleave(function() {
-		elw_enterTimes[this.id] = null;
-	});
-	jQuery(".elw_dialogTrigger").click(function() {
+    elw_enterTimes[this.id] = new Date().getTime();
+    setTimeout("elw_mouseHovered('"+this.id+"')", 600);
+  });
+  jQexpand.mouseleave(function() {
+    elw_enterTimes[this.id] = null;
+  });
+  jQuery(".elw_dialogTrigger").click(function() {
   var thisJQ = jQuery(this);
   jQuery("."+thisJQ.attr("id")).dialog({
-			modal: true,
-			minWidth: 600,
-			minHeight: 400,
-			title: thisJQ.text()
-		});
-	});
-	jQuery(".popupTrigger").mouseleave(function() {
-		jQuery("." + this.id).slideUp(0);
-		jQuery("[class^=" + this.id + "]").slideUp(0);
-	});
-	jQuery("form[method=POST]").submit(function() {
-		var visElemIds = elw_expandedTriggers();
-		jQuery(this).children("input[type=hidden][name=expandTriggers]").val(jQuery.toJSON(visElemIds));
+      modal: true,
+      minWidth: 600,
+      minHeight: 400,
+      title: thisJQ.text()
+    });
+  });
+  jQuery(".popupTrigger").mouseleave(function() {
+    jQuery("." + this.id).slideUp(0);
+    jQuery("[class^=" + this.id + "]").slideUp(0);
+  });
+  jQuery("form[method=POST]").submit(function() {
+    var visElemIds = elw_expandedTriggers();
+    jQuery(this).children("input[type=hidden][name=expandTriggers]").val(jQuery.toJSON(visElemIds));
   jQuery(this).find("input[type=submit]").attr("disabled", "true");
   jQuery(this).find("button[type=submit]").attr("disabled", "true");
-	});
+  });
 });
--- web/src/main/webapp/js/jquery.codecs.js
+++ web/src/main/webapp/js/jquery.codecs.js
@@ -21,151 +21,151 @@
  *	http://www.webtoolkit.info/
  **/
 (function() {
-	function _utf8_encode(string) {
-		string = string.replace(/\r\n/g, "\n");
-		var utftext = "";
+  function _utf8_encode(string) {
+    string = string.replace(/\r\n/g, "\n");
+    var utftext = "";
 
-		for (var n = 0; n < string.length; n++) {
+    for (var n = 0; n < string.length; n++) {
 
-			var c = string.charCodeAt(n);
+      var c = string.charCodeAt(n);
 
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			}
-			else if ((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-		}
+      if (c < 128) {
+        utftext += String.fromCharCode(c);
+      }
+      else if ((c > 127) && (c < 2048)) {
+        utftext += String.fromCharCode((c >> 6) | 192);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+      else {
+        utftext += String.fromCharCode((c >> 12) | 224);
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+    }
 
-		return utftext;
-	}
+    return utftext;
+  }
 
-	function _utf8_decode(utftext) {
-		var string = "";
-		var i = 0;
-		var c, c1, c2;
-		c = c1 = c2 = 0;
+  function _utf8_decode(utftext) {
+    var string = "";
+    var i = 0;
+    var c, c1, c2;
+    c = c1 = c2 = 0;
 
-		while (i < utftext.length) {
+    while (i < utftext.length) {
 
-			c = utftext.charCodeAt(i);
+      c = utftext.charCodeAt(i);
 
-			if (c < 128) {
-				string += String.fromCharCode(c);
-				i++;
-			}
-			else {
-				if ((c > 191) && (c < 224)) {
-					c2 = utftext.charCodeAt(i + 1);
-					string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-					i += 2;
-				}
-				else {
-					c2 = utftext.charCodeAt(i + 1);
-					c3 = utftext.charCodeAt(i + 2);
-					string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-					i += 3;
-				}
-			}
-		}
+      if (c < 128) {
+        string += String.fromCharCode(c);
+        i++;
+      }
+      else {
+        if ((c > 191) && (c < 224)) {
+          c2 = utftext.charCodeAt(i + 1);
+          string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+          i += 2;
+        }
+        else {
+          c2 = utftext.charCodeAt(i + 1);
+          c3 = utftext.charCodeAt(i + 2);
+          string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+          i += 3;
+        }
+      }
+    }
 
-		return string;
-	}
+    return string;
+  }
 
-	var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=";
+  var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=";
 
-	$.extend({
-		encode64 : function (input) {
-			var output = "";
-			var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-			var i = 0;
+  $.extend({
+    encode64 : function (input) {
+      var output = "";
+      var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+      var i = 0;
 
-			input = _utf8_encode(input);
+      input = _utf8_encode(input);
 
-			while (i < input.length) {
+      while (i < input.length) {
 
-				chr1 = input.charCodeAt(i++);
-				chr2 = input.charCodeAt(i++);
-				chr3 = input.charCodeAt(i++);
+        chr1 = input.charCodeAt(i++);
+        chr2 = input.charCodeAt(i++);
+        chr3 = input.charCodeAt(i++);
 
-				enc1 = chr1 >> 2;
-				enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-				enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-				enc4 = chr3 & 63;
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
 
-				if (isNaN(chr2)) {
-					enc3 = enc4 = 64;
-				} else {
-					if (isNaN(chr3)) {
-						enc4 = 64;
-					}
-				}
+        if (isNaN(chr2)) {
+          enc3 = enc4 = 64;
+        } else {
+          if (isNaN(chr3)) {
+            enc4 = 64;
+          }
+        }
 
-				output = output +
-						_keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
-						_keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+        output = output +
+            _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+            _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
 
-			}
+      }
 
-			return output;
-		},
+      return output;
+    },
 
-		// public method for decoding
-		decode64 : function (input) {
-			var output = "";
-			var chr1, chr2, chr3;
-			var enc1, enc2, enc3, enc4;
-			var i = 0;
+    // public method for decoding
+    decode64 : function (input) {
+      var output = "";
+      var chr1, chr2, chr3;
+      var enc1, enc2, enc3, enc4;
+      var i = 0;
 
-			input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+      input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
-			while (i < input.length) {
+      while (i < input.length) {
 
-				enc1 = _keyStr.indexOf(input.charAt(i++));
-				enc2 = _keyStr.indexOf(input.charAt(i++));
-				enc3 = _keyStr.indexOf(input.charAt(i++));
-				enc4 = _keyStr.indexOf(input.charAt(i++));
+        enc1 = _keyStr.indexOf(input.charAt(i++));
+        enc2 = _keyStr.indexOf(input.charAt(i++));
+        enc3 = _keyStr.indexOf(input.charAt(i++));
+        enc4 = _keyStr.indexOf(input.charAt(i++));
 
-				chr1 = (enc1 << 2) | (enc2 >> 4);
-				chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-				chr3 = ((enc3 & 3) << 6) | enc4;
+        chr1 = (enc1 << 2) | (enc2 >> 4);
+        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+        chr3 = ((enc3 & 3) << 6) | enc4;
 
-				output = output + String.fromCharCode(chr1);
+        output = output + String.fromCharCode(chr1);
 
-				if (enc3 != 64) {
-					output = output + String.fromCharCode(chr2);
-				}
-				if (enc4 != 64) {
-					output = output + String.fromCharCode(chr3);
-				}
+        if (enc3 != 64) {
+          output = output + String.fromCharCode(chr2);
+        }
+        if (enc4 != 64) {
+          output = output + String.fromCharCode(chr3);
+        }
 
-			}
+      }
 
-			output = _utf8_decode(output);
+      output = _utf8_decode(output);
 
-			return output;
-		},
-		URLEncode:function(string) {
-			if (typeof encodeURIComponent == 'function') {
-				return encodeURIComponent(string);
-			} else {
-				return escape(_utf8_encode(string));	// pre-1,5ish way
-			}
-		},
-		URLDecode:function(string) {
-			if (typeof decodeURIComponent == 'function') {
-				return decodeURIComponent(string);
-			} else {
-				return _utf8_decode(unescape(string));
-			}
-		}
-	});
+      return output;
+    },
+    URLEncode:function(string) {
+      if (typeof encodeURIComponent == 'function') {
+        return encodeURIComponent(string);
+      } else {
+        return escape(_utf8_encode(string));	// pre-1,5ish way
+      }
+    },
+    URLDecode:function(string) {
+      if (typeof decodeURIComponent == 'function') {
+        return decodeURIComponent(string);
+      } else {
+        return _utf8_decode(unescape(string));
+      }
+    }
+  });
 })();
 
 /*
@@ -184,184 +184,184 @@
  * copyrighted 2005 by Bob Ippolito.
  */
 (function($) {
-	/** jQuery.toJSON( json-serializble )
-	 Converts the given argument into a JSON respresentation.
+  /** jQuery.toJSON( json-serializble )
+   Converts the given argument into a JSON respresentation.
 
-	 If an object has a "toJSON" function, that will be used to get the representation.
-	 Non-integer/string keys are skipped in the object, as are keys that point to a function.
+   If an object has a "toJSON" function, that will be used to get the representation.
+   Non-integer/string keys are skipped in the object, as are keys that point to a function.
 
-	 json-serializble:
-	 The *thing* to be converted.
-	 **/
-	$.toJSON = function(o) {
-		if (typeof(JSON) == 'object' && JSON.stringify) {
-			return JSON.stringify(o);
-		}
+   json-serializble:
+   The *thing* to be converted.
+   **/
+  $.toJSON = function(o) {
+    if (typeof(JSON) == 'object' && JSON.stringify) {
+      return JSON.stringify(o);
+    }
 
-		var type = typeof(o);
-		if (o === null) {
-			return "null";
-		}
+    var type = typeof(o);
+    if (o === null) {
+      return "null";
+    }
 
-		if (type == "undefined") {
-			return undefined;
-		}
+    if (type == "undefined") {
+      return undefined;
+    }
 
-		if (type == "number" || type == "boolean") {
-			return o + "";
-		}
+    if (type == "number" || type == "boolean") {
+      return o + "";
+    }
 
-		if (type == "string") {
-			return $.quoteString(o);
-		}
+    if (type == "string") {
+      return $.quoteString(o);
+    }
 
-		if (type == 'object') {
-			if (typeof o.toJSON == "function") {
-				return $.toJSON(o.toJSON());
-			}
+    if (type == 'object') {
+      if (typeof o.toJSON == "function") {
+        return $.toJSON(o.toJSON());
+      }
 
-			if (o.constructor === Date) {
-				var month = o.getUTCMonth() + 1;
-				if (month < 10) {
-					month = '0' + month;
-				}
+      if (o.constructor === Date) {
+        var month = o.getUTCMonth() + 1;
+        if (month < 10) {
+          month = '0' + month;
+        }
 
-				var day = o.getUTCDate();
-				if (day < 10) {
-					day = '0' + day;
-				}
+        var day = o.getUTCDate();
+        if (day < 10) {
+          day = '0' + day;
+        }
 
-				var year = o.getUTCFullYear();
+        var year = o.getUTCFullYear();
 
-				var hours = o.getUTCHours();
-				if (hours < 10) {
-					hours = '0' + hours;
-				}
+        var hours = o.getUTCHours();
+        if (hours < 10) {
+          hours = '0' + hours;
+        }
 
-				var minutes = o.getUTCMinutes();
-				if (minutes < 10) {
-					minutes = '0' + minutes;
-				}
+        var minutes = o.getUTCMinutes();
+        if (minutes < 10) {
+          minutes = '0' + minutes;
+        }
 
-				var seconds = o.getUTCSeconds();
-				if (seconds < 10) {
-					seconds = '0' + seconds;
-				}
+        var seconds = o.getUTCSeconds();
+        if (seconds < 10) {
+          seconds = '0' + seconds;
+        }
 
-				var milli = o.getUTCMilliseconds();
-				if (milli < 100) {
-					milli = '0' + milli;
-				}
-				if (milli < 10) {
-					milli = '0' + milli;
-				}
+        var milli = o.getUTCMilliseconds();
+        if (milli < 100) {
+          milli = '0' + milli;
+        }
+        if (milli < 10) {
+          milli = '0' + milli;
+        }
 
-				return '"' + year + '-' + month + '-' + day + 'T' +
-						hours + ':' + minutes + ':' + seconds +
-						'.' + milli + 'Z"';
-			}
+        return '"' + year + '-' + month + '-' + day + 'T' +
+            hours + ':' + minutes + ':' + seconds +
+            '.' + milli + 'Z"';
+      }
 
-			if (o.constructor === Array) {
-				var ret = [];
-				for (var i = 0; i < o.length; i++) {
-					ret.push($.toJSON(o[i]) || "null");
-				}
+      if (o.constructor === Array) {
+        var ret = [];
+        for (var i = 0; i < o.length; i++) {
+          ret.push($.toJSON(o[i]) || "null");
+        }
 
-				return "[" + ret.join(",") + "]";
-			}
+        return "[" + ret.join(",") + "]";
+      }
 
-			var pairs = [];
-			for (var k in o) {
-				var name;
-				var type = typeof k;
+      var pairs = [];
+      for (var k in o) {
+        var name;
+        var type = typeof k;
 
-				if (type == "number") {
-					name = '"' + k + '"';
-				} else {
-					if (type == "string") {
-						name = $.quoteString(k);
-					} else {
-						continue;
-					}
-				}  //skip non-string or number keys
+        if (type == "number") {
+          name = '"' + k + '"';
+        } else {
+          if (type == "string") {
+            name = $.quoteString(k);
+          } else {
+            continue;
+          }
+        }  //skip non-string or number keys
 
-				if (typeof o[k] == "function") {
-					continue;
-				}  //skip pairs where the value is a function.
+        if (typeof o[k] == "function") {
+          continue;
+        }  //skip pairs where the value is a function.
 
-				var val = $.toJSON(o[k]);
+        var val = $.toJSON(o[k]);
 
-				pairs.push(name + ":" + val);
-			}
+        pairs.push(name + ":" + val);
+      }
 
-			return "{" + pairs.join(", ") + "}";
-		}
-	};
+      return "{" + pairs.join(", ") + "}";
+    }
+  };
 
-	/** jQuery.evalJSON(src)
-	 Evaluates a given piece of json source.
-	 **/
-	$.evalJSON = function(src) {
-		if (typeof(JSON) == 'object' && JSON.parse) {
-			return JSON.parse(src);
-		}
-		return eval("(" + src + ")");
-	};
+  /** jQuery.evalJSON(src)
+   Evaluates a given piece of json source.
+   **/
+  $.evalJSON = function(src) {
+    if (typeof(JSON) == 'object' && JSON.parse) {
+      return JSON.parse(src);
+    }
+    return eval("(" + src + ")");
+  };
 
-	/** jQuery.secureEvalJSON(src)
-	 Evals JSON in a way that is *more* secure.
-	 **/
-	$.secureEvalJSON = function(src) {
-		if (typeof(JSON) == 'object' && JSON.parse) {
-			return JSON.parse(src);
-		}
+  /** jQuery.secureEvalJSON(src)
+   Evals JSON in a way that is *more* secure.
+   **/
+  $.secureEvalJSON = function(src) {
+    if (typeof(JSON) == 'object' && JSON.parse) {
+      return JSON.parse(src);
+    }
 
-		var filtered = src;
-		filtered = filtered.replace(/\\["\\\/bfnrtu]/g, '@');
-		filtered = filtered.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
-		filtered = filtered.replace(/(?:^|:|,)(?:\s*\[)+/g, '');
+    var filtered = src;
+    filtered = filtered.replace(/\\["\\\/bfnrtu]/g, '@');
+    filtered = filtered.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
+    filtered = filtered.replace(/(?:^|:|,)(?:\s*\[)+/g, '');
 
-		if (/^[\],:{}\s]*$/.test(filtered)) {
-			return eval("(" + src + ")");
-		} else {
-			throw new SyntaxError("Error parsing JSON, source is not valid.");
-		}
-	};
+    if (/^[\],:{}\s]*$/.test(filtered)) {
+      return eval("(" + src + ")");
+    } else {
+      throw new SyntaxError("Error parsing JSON, source is not valid.");
+    }
+  };
 
-	/** jQuery.quoteString(string)
-	 Returns a string-repr of a string, escaping quotes intelligently.
-	 Mostly a support function for toJSON.
+  /** jQuery.quoteString(string)
+   Returns a string-repr of a string, escaping quotes intelligently.
+   Mostly a support function for toJSON.
 
-	 Examples:
-	 >>> jQuery.quoteString("apple")
-	 "apple"
+   Examples:
+   >>> jQuery.quoteString("apple")
+   "apple"
 
-	 >>> jQuery.quoteString('"Where are we going?", she asked.')
-	 "\"Where are we going?\", she asked."
-	 **/
-	$.quoteString = function(string) {
-		if (string.match(_escapeable)) {
-			return '"' + string.replace(_escapeable, function (a) {
-				var c = _meta[a];
-				if (typeof c === 'string') {
-					return c;
-				}
-				c = a.charCodeAt();
-				return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
-			}) + '"';
-		}
-		return '"' + string + '"';
-	};
+   >>> jQuery.quoteString('"Where are we going?", she asked.')
+   "\"Where are we going?\", she asked."
+   **/
+  $.quoteString = function(string) {
+    if (string.match(_escapeable)) {
+      return '"' + string.replace(_escapeable, function (a) {
+        var c = _meta[a];
+        if (typeof c === 'string') {
+          return c;
+        }
+        c = a.charCodeAt();
+        return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
+      }) + '"';
+    }
+    return '"' + string + '"';
+  };
 
-	var _escapeable = /["\\\x00-\x1f\x7f-\x9f]/g;
+  var _escapeable = /["\\\x00-\x1f\x7f-\x9f]/g;
 
-	var _meta = {
-		'\b': '\\b',
-		'\t': '\\t',
-		'\n': '\\n',
-		'\f': '\\f',
-		'\r': '\\r',
-		'"' : '\\"',
-		'\\': '\\\\'
-	};
+  var _meta = {
+    '\b': '\\b',
+    '\t': '\\t',
+    '\n': '\\n',
+    '\f': '\\f',
+    '\r': '\\r',
+    '"' : '\\"',
+    '\\': '\\\\'
+  };
 })(jQuery);
